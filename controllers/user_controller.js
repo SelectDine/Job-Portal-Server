e394bf7ad0e87d2f587dd5a8eeb9bb9efd0b414c
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-10-12 03:54:11 +0530

    Added user controller for decoding the token and fetching the details of a user

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,39 @@
 /**
  * Created by Yash 1300 on 11-10-2018.
  */
+
+const UserTransactions = require('../database/users/user_db_transactions');
+const Promise = require('bluebird');
+const jwt = require('jsonwebtoken');
+
+module.exports.verify_user_token = (token, secret) => {
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, secret, (err, decoded) => {
+            if (err) {
+                console.error(err);
+                reject({success: false, message: "Error decoding the token"});
+            } else {
+                if (!decoded)
+                    reject({success: false, message: "Corrupt token provided"});
+                else
+                    resolve({success: true, message: "Token decoded successfully", decoded: decoded});
+            }
+        });
+    });
+};
+
+module.exports.fetch_user_details = (user_id) => {
+    return new Promise((resolve, reject) => {
+        UserTransactions.find_user_by_id(user_id, (err, output_user) => {
+            if (err) {
+                console.error(err);
+                reject({success: false, message: "An error occurred"});
+            } else {
+                if (!output_user)
+                    reject({success: false, message: "User not found"});
+                else
+                    resolve({success: true, message: "User details fetched successfully", user: output_user});
+            }
+        });
+    });
+};
