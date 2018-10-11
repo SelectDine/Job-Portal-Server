c85fdc92553512401167dc32cbcbd8fdbfa7bd9d
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-10-12 02:15:00 +0530

    Created route for signing up user

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,3 +1,18 @@
 /**
  * Created by Yash 1300 on 11-10-2018.
  */
+
+const AuthController = require('../controllers/auth_controller');
+const router = require('express').Router();
+
+router.post('/user/signup', (req, res) => {
+    let name = req.body.name;
+    let contact = req.body.contact;
+    let email = req.body.email;
+
+    AuthController.register_user(name, contact, email)
+        .then(data => res.json(data))
+        .catch(err => res.json(err));
+});
+
+module.exports = router;
