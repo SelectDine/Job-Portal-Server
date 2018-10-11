bc39a797f6338589947a2005c60b2133c98cca66
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-10-12 03:32:58 +0530

    Added transactions for compaing hashed password and generating token

--- database/users/user_db_transactions.js
+++ database/users/user_db_transactions.js
@@ -4,6 +4,7 @@
 
 const User = require('./user_schema');
 const bcrypt = require('bcrypt-nodejs');
+const jwt = require('jsonwebtoken');
 
 module.exports.create_user = (name, contact, email, password, next) => {
     let user = new User({
@@ -28,4 +29,16 @@ module.exports.create_user = (name, contact, email, password, next) => {
 
 module.exports.update_resume = (user_id, applying_for, experiences, next) => {
     User.findOneAndUpdate({_id: user_id}, {applying_for: applying_for, experiences: experiences}).exec(next);
+};
+
+module.exports.find_user_by_email = (email, next) => {
+    User.findOne({email: email}).exec(next);
+};
+
+module.exports.compare_password = (user, password, next) => {
+    bcrypt.compare(password, user.password, (err, valid_password) => err ? next(err, null) : next(null, valid_password));
+};
+
+module.exports.generate_token = (user, secret) => {
+    return jwt.sign(JSON.parse(JSON.stringify(user)), secret);
 };
