a13cf6433391717c52a89414cc5861e9cc3d18f2
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-10-12 03:54:51 +0530

    Added route for fetching user details

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,3 +1,27 @@
 /**
  * Created by Yash 1300 on 11-10-2018.
  */
+
+const UserController = require('../controllers/user_controller');
+const router = require('express').Router();
+
+router.use((req, res, next) => {
+    let token = req.headers["x-access-token"];
+    let jwt_secret = process.env.JWT_SECRET;
+    UserController.verify_user_token(token, jwt_secret)
+        .then(data => {
+            req.decoded = data.decoded;
+            next();
+        })
+        .catch(err => res.json(err));
+});
+
+router.get('/details', (req, res) => {
+    let user_id = req.decoded._id;
+
+    UserController.fetch_user_details(user_id)
+        .then(data => res.json(data))
+        .catch(err => res.json(err));
+});
+
+module.exports = router;
