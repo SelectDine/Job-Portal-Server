efa83364e1a83c634a2234cc42a952815d78fda9
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-10-12 02:13:53 +0530

    Added middleware for create a user in DB

--- database/users/user_db_transactions.js
+++ database/users/user_db_transactions.js
@@ -1,3 +1,27 @@
 /**
  * Created by Yash 1300 on 11-10-2018.
  */
+
+const User = require('./user_schema');
+const bcrypt = require('bcrypt-nodejs');
+
+module.exports.create_user = (name, contact, email, password, next) => {
+    let user = new User({
+        name: name,
+        contact: contact,
+        email: email,
+        password: password
+    });
+
+    // Hashing the password
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err)
+            return next(err);
+        bcrypt.hash(user.password, salt, null, (err, hash) => {
+            if (err)
+                return next(err);
+            user.password = hash;
+            user.save(next);
+        });
+    });
+};
